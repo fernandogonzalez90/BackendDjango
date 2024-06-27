a3887b8986d1c66afb711f27353ec7271e6ebb85
fernando <fer.ema.gonzalez@gmail.com>
2024-06-27 17:00:33 -0300

    STATICS_URL

--- Backend/settings.py
+++ Backend/settings.py
@@ -126,9 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # REST_FRAMEWORK = {
 #     'DEFAULT_RENDERER_CLASSES': [
