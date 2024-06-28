24fcbf6930e41f3aff5bcc7f150cc752718c4799
fernando <fer.ema.gonzalez@gmail.com>
2024-06-28 15:04:35 -0300

    cambios en urls

--- Backend/urls.py
+++ Backend/urls.py
@@ -7,8 +7,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('API.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
+]+static(settings.STATIC_URL,document_root=settings.STATICFILES_DIRS)
