b9e23e54457285f68633e215ee02ec4465157474
fernando <fer.ema.gonzalez@gmail.com>
2024-06-28 17:32:21 -0300

    elimino app obsoleta

--- Backend/settings.py
+++ Backend/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.static.StaticMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
