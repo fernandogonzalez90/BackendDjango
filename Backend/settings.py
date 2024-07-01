3f56dc1c157dbd15ed8fcf760f3b15c0b7bc3fee
fernando <fer.ema.gonzalez@gmail.com>
2024-07-01 17:10:42 -0300

    update

--- Backend/settings.py
+++ Backend/settings.py
@@ -142,10 +142,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
