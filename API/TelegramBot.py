f960a079591043f9c8026daea35bbe1105364260
fernando <fer.ema.gonzalez@gmail.com>
2024-07-19 13:27:03 -0300

    Corregido errores en bot telegram

--- API/TelegramBot.py
+++ API/TelegramBot.py
@@ -1,9 +1,21 @@
+import asyncio
 from telegram import Bot
-from .obtiene_id_telegram import obtener_id
+from obtiene_id_telegram import obtener_id
+
+message = "probando bot"
 
 
-def send_telegram_message(message):
+async def send_telegram_message(message):
     bot_token = '6764761192:AAEbqx6i77597a6gEEDRGZbqJCBWX4mZ0fA'
     chat_id = obtener_id()
+    print(type(chat_id), chat_id)
     bot = Bot(token=bot_token)
-    bot.send_message(chat_id=chat_id, text=message)
+    await bot.send_message(chat_id=chat_id, text=message)
+    print(f'id: {chat_id}, mensaje: {message}')
+
+
+async def main():
+    await send_telegram_message(message)
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- API/obtiene_id_telegram.py
+++ API/obtiene_id_telegram.py
@@ -2,8 +2,10 @@ import requests
 
 
 def obtener_id():
-    bot_token = '6764761192:AAEbqx6i77597a6gEEDRGZbqJCBWX4mZ0fA'  # Reemplaza con el token de tu bot
+    bot_token = '6764761192:AAEbqx6i77597a6gEEDRGZbqJCBWX4mZ0fA'
     response = requests.get(f'https://api.telegram.org/bot{bot_token}/getUpdates')
     data = response.json()
-    chat_id = data['result'][0]['message']['chat']['id']
+    if not data['result']:
+        raise ValueError("No se encontraron actualizaciones recientes. Asegúrate de haber enviado un mensaje al bot.")
+    chat_id = data['result'][-1]['message']['chat']['id']
     return chat_id
