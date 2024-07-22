bec27f3a285e88a109a26928b04ecc425936f940
fernando <fer.ema.gonzalez@gmail.com>
2024-07-22 15:08:17 -0300

    feat(config): move token to environment variable and handle chat_id file

--- API/obtiene_id_telegram.py
+++ API/obtiene_id_telegram.py
@@ -1,11 +1,19 @@
+import os
 import requests
 
+
 def obtener_id():
-    bot_token = '6764761192:AAEbqx6i77597a6gEEDRGZbqJCBWX4mZ0fA'
-    response = requests.get(f'https://api.telegram.org/bot{bot_token}/getUpdates')
+    bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+    response = requests.get(
+        f'https://api.telegram.org/bot{bot_token}/getUpdates')
+    directorio_script = os.path.dirname(os.path.abspath(__file__))
+    ruta_archivo = os.path.join(directorio_script, 'chat_id.txt')
     data = response.json()
     if not data['result']:
-        raise ValueError("No se encontraron actualizaciones recientes. Asegúrate de haber enviado un mensaje al bot.")
+        raise ValueError(
+            "No se encontraron actualizaciones recientes. Asegúrate de haber enviado un mensaje al bot.")
     chat_id = data['result'][-1]['message']['chat']['id']
-    return chat_id
+    with open(ruta_archivo, 'w') as file:
+        file.write(str(chat_id))
 
+obtener_id()
