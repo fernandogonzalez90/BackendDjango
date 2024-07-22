3f3d8631568c0f627dfeeecffbdafb446a9f7793
fernando <fer.ema.gonzalez@gmail.com>
2024-07-22 15:11:28 -0300

    fix(chat_id): read from chat_id.txt and call obtener_id() if file not found

--- API/TelegramBot.py
+++ API/TelegramBot.py
@@ -1,13 +1,30 @@
 import requests
-from .obtiene_id_telegram import obtener_id
+from obtiene_id_telegram import obtener_id
+import os
+
+
+def chat_id():
+    directorio_script = os.path.dirname(os.path.abspath(__file__))
+    ruta_archivo = os.path.join(directorio_script, 'chat_id.txt')
+    if os.path.exists(ruta_archivo):
+        with open(ruta_archivo, 'r') as file:
+            f = file.read().strip()
+            return f
+        
+    else:
+        obtener_id()
+        return chat_id()
 
 
 def send_telegram_message(message):
-    bot_token = '6764761192:AAEbqx6i77597a6gEEDRGZbqJCBWX4mZ0fA'
-    chat_id = obtener_id()
-    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
-    payload = {'chat_id': chat_id, 'text': message}
-    response = requests.post(url, data=payload)
-    response.raise_for_status()  # Lanzará una excepción si la respuesta tiene un error HTTP
-    print(f'id: {chat_id}, mensaje: {message}')
+    chat_id_value = chat_id()
+    bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
 
+    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+    payload = {'chat_id': chat_id_value, 'text': message}
+    response = requests.post(url, data=payload)
+    response.raise_for_status()
+    print(f'id: {chat_id_value}, mensaje: {message}')
+
+
+send_telegram_message('saludos!')
