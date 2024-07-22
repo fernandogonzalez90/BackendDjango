41a033541a81e5809211f56982da13a9b0ef573d
fernando <fer.ema.gonzalez@gmail.com>
2024-07-22 15:55:57 -0300

    refactor: unifica los archivos para obtener id y el bot de Telegram

--- API/TelegramBot.py
+++ API/TelegramBot.py
@@ -1,6 +1,21 @@
 import requests
-from obtiene_id_telegram import obtener_id
 import os
+
+
+def obtener_id():
+    bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+    response = requests.get(
+        f'https://api.telegram.org/bot{bot_token}/getUpdates')
+    directorio_script = os.path.dirname(os.path.abspath(__file__))
+    ruta_archivo = os.path.join(directorio_script, 'chat_id.txt')
+    data = response.json()
+    print(data)
+    if not data['result']:
+        raise ValueError(
+            "No se encontraron actualizaciones recientes. Asegúrate de haber enviado un mensaje al bot.")
+    chat_id = data['result'][-1]['message']['chat']['id']
+    with open(ruta_archivo, 'w') as file:
+        file.write(str(chat_id))
 
 
 def chat_id():
@@ -10,7 +25,6 @@ def chat_id():
         with open(ruta_archivo, 'r') as file:
             f = file.read().strip()
             return f
-        
     else:
         obtener_id()
         return chat_id()
